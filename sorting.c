6c98363a49d25499d23ac7a65ba507c508d36e5e
kowsalyavsb <31506020+kowsalyavsb@users.noreply.github.com>
2018-02-13 14:35:02 +0530

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,s[3],n,temp;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&s[i]);
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		if(s[i]>s[j])
+		{
+		temp=s[i];
+		s[i]=s[j];
+		s[j]=temp;
+		}
+		}
+	}
+		for(i=0;i<n;i++)
+		printf("%d\n",s[i]);
+	return 0;
+}
