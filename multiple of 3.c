c9ed11b6e66762f9425494f2564c93211919a247
kowsalyavsb <31506020+kowsalyavsb@users.noreply.github.com>
2018-02-27 15:49:28 +0530

    Create multiple of 3.c

---
+++ multiple of 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int k;
+	scanf("%d",&k);
+	if(k%13==0)
+	{
+		printf("the given number is multiple of 13");
+	}
+	else
+	{
+		printf("the given number is not multiple of 13");
+	}
+	return 0;
+}
